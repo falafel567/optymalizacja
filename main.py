e017122ebb59092840bd0aaf9b0671cf8a4c4a19
blazejkuliszak <rickrickardo2137@gmail.com>
2026-04-05 21:50:01 +0200

    algorytm zachłanny i tworzenie populacji

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
+import random
+
 def wczytaj():
-    p = int(input())
-    n = int(input())
-    arr = []
+    with open("dane.txt", "r") as file:
+        p = int(file.readline())
+        n = int(file.readline())
+        tablica = []
+        for _ in range(n):
+            liczba = int(file.readline())
+            tablica.append(liczba)
 
-    for _ in range(n):
-        arr.append(int(input()))
+    print(f"p: {p}, n: {n}")
+    print(f"Tablica: {tablica}\n")
+    return p, tablica
 
-    return p, arr
+def populacja_pierwsza(tablica):
+    osobnik = random.sample(tablica, len(tablica))
+    print(f"osobnik: {osobnik}")
+    return osobnik
+
+def zachlanny(osobnik, p):
+    t_koniec = [osobnik[i] for i in range(p)]
+    #print(f"t_koniec: {t_koniec}\n")
+    for zadanie in range(p, len(osobnik)):
+        t_koniec[t_koniec.index(min(t_koniec))] += osobnik[zadanie]
+        #print(t_koniec)
+    return max(t_koniec)
+
 
 if __name__ == '__main__':
-    p, arr = wczytaj()
-    print(p)
-    print(arr)
+    p, tablica = wczytaj()
+    osobniki = []
+    for i in range(p):
+        osobniki.append(populacja_pierwsza(tablica))
+    print(f"osobniki: {osobniki}")
+    t_max = []
+    for osobnik in osobniki:
+        t_max.append((zachlanny(osobnik, p), osobnik))
+        print("\n")
+    t_max.sort(key=lambda x: x[0])
+    print(t_max)
 
