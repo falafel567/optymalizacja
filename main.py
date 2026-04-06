e385ea7eed7dc21790ae11e414caf13a6233bbe5
blazejkuliszak <rickrickardo2137@gmail.com>
2026-04-06 12:26:11 +0200

    testowy to aktualny nowy main

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import random
 import math
+import copy
+
+liczba_pokolen = 5000
+rozmiar_populacji = 50
+szansa_mutacji = 0.2
 
 def wczytaj():
     with open("test.txt", "r") as file:
@@ -26,28 +31,62 @@ def zachlanny(osobnik, p, tablica):
     return max(t_koniec)
 
 def krzyzowanie(osobnik1, osobnik2):
-    swap1 = random.randint(0, len(osobnik1) - 1)
-    swap2 = random.randint(0, len(osobnik1) - 1)
-    while swap1 == swap2:
-        swap2 = random.randint(0, len(osobnik1) - 1)
-    osobnik_new = osobnik1[min(swap1, swap2):max(swap1, swap2)]
+    swap1 = random.randint(0, len(osobnik1)-1)
+    swap2 = random.randint(0, len(osobnik1)-1)
+    while swap1 == swap2 or (swap1 == 0 and swap2 == len(osobnik1)-1) or (swap2 == 0 and swap1 == len(osobnik1)-1):
+        swap2 = random.randint(0, len(osobnik1)-1)
+    #print(swap1, swap2)
+    osobnik1_new = ['_' for _ in range(len(osobnik1))]
     wyjatki = set()
     for x in range(min(swap1, swap2), max(swap1, swap2)):
+        osobnik1_new[x] = osobnik1[x]
         wyjatki.add(osobnik1[x])
-    for x in range(0, len(osobnik2)):
-        if osobnik2[x] not in wyjatki:
-            osobnik_new.append(osobnik2[x])
+    #print(osobnik1_new)
+    osobnik_copy = copy.deepcopy(osobnik2)
+    #print(osobnik_copy)
+    to_del = []
+    for ind in range(len(osobnik1_new)):
+        if osobnik_copy[ind] in wyjatki:
+           to_del.append(ind)
+    to_del.sort(reverse=True)
+    for usun in to_del:
+        osobnik_copy.pop(usun)
+    for i in range(len(osobnik1_new)):
+        if osobnik1_new[i] == '_':
+            osobnik1_new[i] = osobnik_copy[0]
+            osobnik_copy.pop(0)
+    #print(osobnik1_new)
     del wyjatki
-    return osobnik_new
+
+    osobnik2_new = ['_' for _ in range(len(osobnik1))]
+    wyjatki = set()
+    for x in range(min(swap1, swap2), max(swap1, swap2)):
+        osobnik2_new[x] = osobnik2[x]
+        wyjatki.add(osobnik2[x])
+    #print(osobnik2_new)
+    osobnik_copy = copy.deepcopy(osobnik1)
+    #print(osobnik_copy)
+    to_del = []
+    for ind in range(len(osobnik2_new)):
+        if osobnik_copy[ind] in wyjatki:
+            to_del.append(ind)
+    to_del.sort(reverse=True)
+    for usun in to_del:
+        osobnik_copy.pop(usun)
+    for i in range(len(osobnik2_new)):
+        if osobnik2_new[i] == '_':
+            osobnik2_new[i] = osobnik_copy[0]
+            osobnik_copy.pop(0)
+    #print(osobnik2_new)
+    del wyjatki
+    return osobnik1_new, osobnik2_new
 
 def mutacja(osobnik):
-    swap1 = random.randint(0, len(osobnik) - 1)
-    swap2 = random.randint(0, len(osobnik) - 1)
+    swap1 = random.randint(0, len(osobnik)-1)
+    swap2 = random.randint(0, len(osobnik)-1)
     while swap1 == swap2:
-        swap2 = random.randint(0, len(osobnik) - 1)
-    temp = osobnik[swap1]
-    osobnik[swap1] = osobnik[swap2]
-    osobnik[swap2] = temp
+        swap2 = random.randint(0, len(osobnik)-1)
+    osobnik[swap1], osobnik[swap2] = osobnik[swap2], osobnik[swap1]
     return osobnik
 
 if __name__ == '__main__':
@@ -56,8 +95,8 @@ if __name__ == '__main__':
     t_max = []
     osobniki = []
 
-    #tworzenie nowej populacji o rozmiarze p
-    for i in range(p):
+    #tworzenie nowej populacji o rozmiarze rozmiar_populacji
+    for i in range(rozmiar_populacji):
         osobniki.append(populacja_pierwsza(len(tablica)))
 
     # #algorytm zachlanny
@@ -70,14 +109,34 @@ if __name__ == '__main__':
     do_usuniecia.sort(reverse=True)
     for usun in do_usuniecia:
         osobniki.pop(usun)
+    t_max.clear()
+    do_usuniecia.clear()
 
     #glowna petla programu
-    while len(osobniki) > 1:
-        do_roboty = len(osobniki)
-        for _ in range(0, do_roboty, 2):
-            osobniki.append(krzyzowanie(osobniki[0], osobniki[1]))
-            osobniki.pop(0)
-            osobniki.pop(0)
-        for osobnik in osobniki:
-            osobnik = mutacja(osobnik)
-    print(f"T_max: {zachlanny(osobniki[0], p, tablica)}")
+    i = 0
+    while i < liczba_pokolen:
+        osobniki_nowe = [osobniki[0]]
+        random.shuffle(osobniki)
+        for x in range(0, len(osobniki) - 1, 2):
+            o1, o2 = krzyzowanie(osobniki[x], osobniki[x + 1])
+            osobniki_nowe.extend([o1, o2])
+        if len(osobniki_nowe) != rozmiar_populacji:
+            osobniki_nowe.append(mutacja(osobniki_nowe[0]))
+        for key, osobnik in enumerate(osobniki_nowe):
+            if random.random() < szansa_mutacji:
+                osobniki_nowe[key] = mutacja(osobnik)
+        osobniki = copy.deepcopy(osobniki_nowe)
+        for key, osobnik in enumerate(osobniki):
+            t_max.append((zachlanny(osobnik, p, tablica), key))
+        t_max.sort(key=lambda x: x[0])
+        do_usuniecia = []
+        for x in range(math.ceil(len(t_max) / 2), len(t_max)):
+            do_usuniecia.append(t_max[x][1])
+        do_usuniecia.sort(reverse=True)
+        for usun in do_usuniecia:
+            osobniki.pop(usun)
+        if i == liczba_pokolen-1:
+            print(t_max[0][0])
+        t_max.clear()
+        do_usuniecia.clear()
+        i+=1
---
+++ testowy.py
@@ -0,0 +1,104 @@
+import random
+import math
+import copy
+
+# --- PARAMETRY ---
+liczba_pokolen = 10000
+rozmiar_populacji = 120
+szansa_mutacji = 0.20
+elityzm = 2
+
+
+def wczytaj():
+    try:
+        with open("test.txt", "r") as file:
+            p = int(file.readline())
+            n = int(file.readline())
+            tablica = [int(file.readline()) for _ in range(n)]
+        return p, tablica
+    except FileNotFoundError:
+        print("Błąd: Nie znaleziono pliku test.txt")
+        return 2, [10, 20, 30, 40, 50]
+
+
+def populacja_pierwsza(n):
+    tab = list(range(n))
+    return random.sample(tab, n)
+
+
+def zachlanny(osobnik, p, tablica):
+    t_koniec = [0] * p
+    for gene in osobnik:
+        idx_min = t_koniec.index(min(t_koniec))
+        t_koniec[idx_min] += tablica[gene]
+    return max(t_koniec)
+
+
+def krzyzowanie(osobnik1, osobnik2):
+    size = len(osobnik1)
+    a, b = sorted(random.sample(range(size), 2))
+
+    def fill_child(p1, p2):
+        child = ['_'] * size
+        child[a:b] = p1[a:b]
+        wyjatki = set(child[a:b])
+
+        p2_genes = [gene for gene in p2 if gene not in wyjatki]
+
+        idx = 0
+        for i in range(size):
+            if child[i] == '_':
+                child[i] = p2_genes[idx]
+                idx += 1
+        return child
+
+    return fill_child(osobnik1, osobnik2)
+
+
+def mutacja(osobnik):
+    nowy = osobnik[:]
+    idx1, idx2 = random.sample(range(len(nowy)), 2)
+    nowy[idx1], nowy[idx2] = nowy[idx2], nowy[idx1]
+    return nowy
+
+
+def selekcja_turniejowa(populacja_z_fitness, k=3):
+    turniej = random.sample(populacja_z_fitness, k)
+    turniej.sort(key=lambda x: x[0])
+    return copy.deepcopy(turniej[0][1])
+
+
+if __name__ == '__main__':
+    p, tablica = wczytaj()
+    n = len(tablica)
+
+    osobniki = [populacja_pierwsza(n) for _ in range(rozmiar_populacji)]
+
+    for gen_idx in range(liczba_pokolen):
+        oceniona_populacja = []
+        for o in osobniki:
+            oceniona_populacja.append((zachlanny(o, p, tablica), o))
+
+        oceniona_populacja.sort(key=lambda x: x[0])
+
+        nowa_populacja = []
+
+        for i in range(elityzm):
+            nowa_populacja.append(copy.deepcopy(oceniona_populacja[i][1]))
+
+        while len(nowa_populacja) < rozmiar_populacji:
+            rodzic1 = selekcja_turniejowa(oceniona_populacja)
+            rodzic2 = selekcja_turniejowa(oceniona_populacja)
+
+            dziecko = krzyzowanie(rodzic1, rodzic2)
+
+            if random.random() < szansa_mutacji:
+                dziecko1 = mutacja(dziecko)
+
+            nowa_populacja.append(dziecko)
+
+        osobniki = nowa_populacja
+
+    finalna_ocena = sorted([(zachlanny(o, p, tablica), o) for o in osobniki])
+    print("--- KONIEC ---")
+    print(f"Najlepszy znaleziony czas: {finalna_ocena[0][0]}")
